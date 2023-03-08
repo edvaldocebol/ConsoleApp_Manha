3b9d9e9b6fddebfb96d127f4daab14cda2e5136b
EDSON PAULA <ej91720@gmailcom>
2023-03-08 08:56:28 -0300

    Criate the metodo constructor and the class

--- ConsoleApp_Manha/produto.cs
+++ ConsoleApp_Manha/produto.cs
@@ -8,5 +8,16 @@ namespace ConsoleApp_Manha
 {
     internal class produto
     {
+        private int Id;
+        private string Descricao;
+        private decimal Valor;
+
+        public produto(int id, string descricao, decimal valor) {
+
+            Id = id;
+            Descricao = descricao;
+            Valor = valor;
+
+        }
     }
 }
