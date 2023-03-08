79788ac41c14bf13f5103cce91a28da54f7271f8
EDSON PAULA <ej91720@gmailcom>
2023-03-08 10:34:34 -0300

    Tarefa Class Pessoa

---
+++ ConsoleApp_Manha/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Manha
+{
+    public class Pessoa
+    {
+
+        private string Telefone;
+        private int Id;
+
+        public Pessoa(string telefone, int id)
+        {
+            Id = id;
+            Telefone = telefone;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+
+        public void steId(int id)
+        {
+            Id = id;
+        }
+    }
+}
